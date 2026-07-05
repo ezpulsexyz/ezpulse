da8f090fa1a97cd5cc8892f11af598f79247f611
julien12799 <julien12799@gmail.com>
2026-07-06 02:22:29 +0700

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_SUPABASE_URL?:https://wgqkdonexxltajdmergm.supabase.co;
-  readonly VITE_SUPABASE_ANON_KEY?:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndncWtkb25leHhsdGFqZG1lcmdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMyNzIyMDYsImV4cCI6MjA5ODg0ODIwNn0.YSLrxrKYjQVP4npcXZFa22dM6owbWqEEN-eV0HQYebg;
+  readonly VITE_SUPABASE_URL?: https://wgqkdonexxltajdmergm.supabase.co ;
+  readonly VITE_SUPABASE_ANON_KEY?: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndncWtkb25leHhsdGFqZG1lcmdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMyNzIyMDYsImV4cCI6MjA5ODg0ODIwNn0.YSLrxrKYjQVP4npcXZFa22dM6owbWqEEN-eV0HQYebg ;
 }
 
 interface ImportMeta {
